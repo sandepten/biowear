1a7e7dc090e53af2eedf1dd84b947af00d5207b4
Sandeep Kumar <sandepten@gmail.com>
2022-08-02 12:51:29 +0530

    fixed nabar case

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import Footer from "../components/Footer";
-import Navbar from "../components/navbar";
+import Navbar from "../components/Navbar";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
